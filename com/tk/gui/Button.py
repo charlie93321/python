9b5c6b356964cc05c87d88ec522a35162f786831
charlie_wutong <2459060612@qq.com>
2019-11-11 23:53:30 +0800

    Btn改进

--- com/tk/gui/Button.py
+++ com/tk/gui/Button.py
@@ -2,6 +2,206 @@
 # -*- coding: UTF-8 -*-
 
 import Tkinter as tk
+import tkFont
+
+
+class Label():
+    '''
+    text	1. 指定 Label 显示的文本
+        2. 文本可以包含换行符
+        3. 如果设置了 bitmap 或 image 选项，该选项则被忽略
+    bitmap	1. 指定显示到 Label 上的位图
+        2. 如果指定了 image 选项，则该选项被忽略
+    image	1. 指定 Label 显示的图片
+        2. 该值应该是 PhotoImage，BitmapImage，或者能兼容的对象
+        3. 该选项优先于 text 和 bitmap 选项
+
+    textvariable	1. Label 显示 Tkinter 变量（通常是一个 StringVar 变量）的内容
+                2. 如果变量被修改，Label 的文本会自动更新
+    '''
+
+    def __init__(self, parent, image=None, bitmap=None, text=None, textvariable=None):
+        self.parent = parent
+        self.image = image
+        self.bitmap = bitmap
+        self.text = text
+        self.textvariable = textvariable
+        if self.image is not None:
+            self.label = tk.Label(master=parent, image=self.image)
+        elif self.bitmap is not None:
+            self.label = tk.Label(master=parent, bitmap=self.bitmap)
+        elif textvariable is not None:
+            self.label = tk.Label(master=parent, textvariable=self.textvariable)
+        else:
+            self.label = tk.Label(master=parent, text=self.text)
+        self.featureMap = {'parent': parent, 'image': self.image, 'bitmap': self.bitmap, 'text': self.textvariable,
+                           'textvariable': self.textvariable}
+
+    '''
+    
+    anchor	    1. 控制文本（或图像）在 Label 中显示的位置
+                2. "n", "ne", "e", "se", "s", "sw", "w", "nw", 或者 "center" 来定位（ewsn 代表东西南北，上北下南左西右东）
+                     默认值是 "center"
+    bg	跟 background 一样
+     
+    bd	1. 指定 Label 的边框宽度
+                2. 默认值由系统指定，通常是 1 或 2 像素
+    
+    
+    font	1. 指定 Label 中文本的字体(注：如果同时设置字体和大小，应该用元组包起来，如（"楷体", 20）
+            2. 一个 Label 只能设置一种字体
+            3. 默认值由系统指定
+    fg	1. 设置 Label 的文本和位图的颜色
+                2. 默认值由系统指定
+    
+    height	1. 设置 Label 的高度
+            2. 如果 Label 显示的是文本，那么单位是文本单元
+            3. 如果 Label 显示的是图像，那么单位是像素（或屏幕单元）
+            4. 如果设置为 0 或者干脆不设置，那么会自动根据 Label 的内容计算出高度
+    
+     
+    justify	1. 定义如何对齐多行文本
+            2. 使用 "left"，"right" 或 "center"
+            3. 注意，文本的位置取决于 anchor 选项
+            4. 默认值是 "center"
+    padx	1. 指定 Label 水平方向上的额外间距（内容和边框间）
+            2. 单位是像素
+    pady	1. 指定 Label 垂直方向上的额外间距（内容和边框间）
+            2. 单位是像素
+    relief	1. 指定边框样式
+            2. 默认值是 "flat"
+            3. 另外你还可以设置 "groove", "raised", "ridge", "solid" 或者 "sunken"
+    state	1. 指定 Label 的状态
+            2. 这个标签控制 Label 如何显示
+            3. 默认值是 "normal
+            4. 另外你还可以设置 "active" 或 "disabled"
+    
+                 
+    underline	1. 跟 text 选项一起使用，用于指定哪一个字符画下划线（例如用于表示键盘快捷键） 
+                2. 默认值是 -1
+                3. 例如设置为 1，则说明在 Button 的第 2 个字符处画下划线
+    width	1. 设置 Label 的宽度
+            2. 如果 Label 显示的是文本，那么单位是文本单元
+            3. 如果 Label 显示的是图像，那么单位是像素（或屏幕单元）
+            4. 如果设置为 0 或者干脆不设置，那么会自动根据 Label 的内容计算出宽度
+    wraplength	1. 决定 Label 的文本应该被分成多少行
+                2. 该选项指定每行的长度，单位是屏幕单元
+                3. 默认值是 0
+                
+                
+                
+    takefocus	1. 如果是 True，该 Label 接受输入焦点
+    2. 默认值是 False
+    
+    highlightbackground	1. 指定当 Label 没有获得焦点的时候高亮边框的颜色
+            2. 默认值由系统指定，通常是标准背景颜色
+    highlightcolor	1. 指定当 Label 获得焦点的时候高亮边框的颜色
+        2. 默认值由系统指定
+    highlightthickness	1. 指定高亮边框的宽度
+            2. 默认值是 0（不带高亮边框）
+                        
+    compound	1. 控制 Label 中文本和图像的混合模式
+                2. 默认情况下，如果有指定位图或图片，则不显示文本
+                3. 如果该选项设置为 "center"，文本显示在图像上（文本重叠图像）
+                4. 如果该选项设置为 "bottom"，"left"，"right" 或 "top"，那么图像显示在文本的旁边（如 "bottom"，则图像在文本的下方）
+                5. 默认值是 NONE
+    cursor	    1. 指定当鼠标在 Label 上飘过的时候的鼠标样式
+                2. 默认值由系统指定
+    disabledforeground	1. 指定当 Label 不可用的时候前景色的颜色
+                        2. 默认值由系统指定
+    '''
+
+    def value(self, name):
+        return self.featureMap[name]
+
+    def features(self, width=None, height=None, bd=None, fg=None, bg=None, padx=None, pady=None, font=None, relief=None,
+                 underline=None, state=None, justify=None, wraplength=None, anchor=None,
+                 takefocus=False, highlightbackground=None, highlightcolor=None, highlightthickness=0, compound=None,
+                 cursor=None, disabledforeground=None):
+        if width is not None:
+            self.label.configure(width=width)
+            self.featureMap['width'] = width
+        if height is not None:
+            self.label.configure(height=height)
+            self.featureMap['height'] = height
+        if bd is not None:
+            self.label.configure(bd=bd)
+            self.featureMap['bd'] = bd
+        if fg is not None:
+            self.label.configure(fg=fg)
+            self.featureMap['fg'] = fg
+        if bg is not None:
+            self.label.configure(bg=bg)
+            self.featureMap['bg'] = bg
+        if padx is not None:
+            self.label.configure(padx=padx)
+            self.featureMap['padx'] = padx
+        if pady is not None:
+            self.label.configure(pady=pady)
+            self.featureMap['pady'] = pady
+        if font is not None:
+            self.label.configure(font=font)
+            self.featureMap['font'] = font
+        if relief is not None:
+            self.label.configure(relief=relief)
+            self.featureMap['relief'] = relief
+        if underline is not None:
+            self.label.configure(underline=underline)
+            self.featureMap['underline'] = underline
+        if state is not None:
+            self.label.configure(state=state)
+            self.featureMap['state'] = state
+        if justify is not None:
+            self.label.configure(justify=justify)
+            self.featureMap['justify'] = justify
+        if wraplength is not None:
+            self.label.configure(wraplength=wraplength)
+            self.featureMap['wraplength'] = wraplength
+        if anchor is not None:
+            self.label.configure(anchor=anchor)
+            self.featureMap['anchor'] = anchor
+
+        '''
+        takefocus=False,highlightbackground=None,highlightcolor=None,highlightthickness=0,compound=None,cursor=None,disabledforeground=None
+        '''
+        if takefocus is not None:
+            self.label.configure(takefocus=takefocus)
+            self.featureMap['takefocus'] = takefocus
+        if highlightbackground is not None:
+            self.label.configure(highlightbackground=highlightbackground)
+            self.featureMap['highlightbackground'] = highlightbackground
+        if highlightcolor is not None:
+            self.label.configure(highlightcolor=highlightcolor)
+            self.featureMap['highlightcolor'] = highlightcolor
+        if highlightthickness is not None:
+            self.label.configure(highlightthickness=highlightthickness)
+            self.featureMap['highlightthickness'] = highlightthickness
+        if compound is not None:
+            self.label.configure(compound=compound)
+            self.featureMap['compound'] = compound
+        if cursor is not None:
+            self.label.configure(cursor=cursor)
+            self.featureMap['cursor'] = cursor
+        if disabledforeground is not None:
+            self.label.configure(disabledforeground=disabledforeground)
+            self.featureMap['disabledforeground'] = disabledforeground
+
+    '''
+         activebackground	1. 设置当 Label 处于活动状态（通过 state 选项设置状态）的背景色
+                    2. 默认值由系统指定
+        activeforeground	1. 设置当 Label 处于活动状态（通过 state 选项设置状态）的前景色
+                        2. 默认值由系统指定
+    '''
+
+    def activebackground(self, activebackground):
+        if activebackground is not None:
+            self.label.configure(activebackground=self.activebackground)
+            self.featureMap['activebackground'] = activebackground
+
+    def activeforeground(self, activeforeground):
+        if activeforeground is not None:
+            self.label.configure(activeforeground=self.activeforeground)
+            self.featureMap['activeforeground'] = activeforeground
 
 
 class Button():
@@ -124,3 +324,30 @@ class Button():
 
     def select(self):
         self.btn.select()
+
+        '''
+1）family 为字体类型，如family='Times'（新罗马字体）,family=' 微软雅黑'等
+
+2）size 字体大小，字体大小以磅为单位。要以像素为单位给出大小，前面加一个负号
+
+3）weight 是否加粗，默认为NORMAL，如果设置加粗使用weight = tf.BOLD
+
+4）slant 是否斜体，模型为NORAML，如果设置斜体使用slant  = tf.ITALIC  must be roman, or italic
+
+5）underline 是否有下划线，默认为0(false)，如果设置下划线使用underline = 1(true)
+
+6）overstrike 是否有删除线，默认为0（false），如果设置删除线使用overstrike = 1（true）
+        '''
+
+
+class Font():
+    def __init__(self, family='Consolas', size=12, weight=tkFont.NORMAL, slant=tkFont.ROMAN, underline=0,
+                 overstrike=0):
+        self.family = family
+        self.size = size
+        self.weight = weight
+        self.slant = slant
+        self.underline = underline
+        self.overstrike = overstrike
+        self.font = tkFont.Font(family=self.family, size=self.size, weight=self.weight, slant=self.slant,
+                                underline=self.underline, overstrike=self.overstrike)
--- com/tk/gui/demo1.py
+++ com/tk/gui/demo1.py
@@ -2,17 +2,15 @@
 # -*- coding: UTF-8 -*-
 
 import Tkinter as tk
+import random
+import tkFont
 
-from com.tk.gui.Button import Button
+from com.tk.gui.Button import Button, Label, Font
 
 
 def hit_me(target):
-
     print target.value('underline')
-
-
-
-
+    varStr.set(varStr.get()+str(random.randint(1,100)))
 
 
 app = tk.Tk()
@@ -21,11 +19,20 @@ app.title('窗口')
 
 app.geometry('500x300')
 
+btn2 = Button(parent=app, command=hit_me, text='hit me')
+btn2.features(bg='red', width=500, underline=-1, font=Font(family='楷体',size=32,weight=tkFont.BOLD,slant=tkFont.ITALIC).font, fg='green')
 
-btn2= Button(parent=app,command=hit_me,text='hit me')
-btn2.features(bg='red',width=500,underline=1,font=('Consolas',16),fg='green')
+varStr = tk.StringVar()
+varStr.set("Hit me !!!")
 
 
+
+
+lb1 = Label(parent=app, textvariable=varStr)
+
+lb1.features(bg='green',font=('宋体',12),fg='black',width=150)
+
+lb1.label.pack()
 btn2.btn.pack()
 
 app.mainloop()
